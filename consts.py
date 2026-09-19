bad6672c0eff55dc86ed022d0bbd04567c7c7470
Royee <Royeeyosibash@hotmail.com>
2026-09-20 02:34:34 +0300

    Changed pyproject.toml

--- consts.py
+++ consts.py
@@ -1,9 +1,5 @@
-import json
 from pathlib import Path
 
 APP_ROOT = Path(__file__).parent
-
-with open(APP_ROOT / "release_data.json") as _f:
-    __version__ = json.load(_f)["version"]
 
 IMAGE_EXTENSIONS = {".png", ".jpg", ".jpeg", ".bmp", ".tiff", ".tif", ".webp"}
